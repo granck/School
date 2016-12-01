1cb6f7adcaab2ca2419cada9b32503b7fa016a82
Garrick Ranck <Garrick@GEOLAP1111.local>
2016-12-01 09:28:14 -0600

    Added Scanner object and implemented capturing a user number

--- testgit/testChanges.java
+++ testgit/testChanges.java
@@ -1,10 +1,19 @@
-
+import java.util.Scanner;
 
 public class testChanges{
 	
 	public static void main(String[] args){
-
+		
+		Scanner input = new Scanner(System.in);
+		int testNumber = 0;
 		System.out.println("This is the basic program.");
+
+		System.out.print("Type a number: ");
+		testNumber = input.nextInt();
+		
+		System.out.println("You typed: " + testNumber);
+
+		
 
 	}//end main method
 }//end class testChanges
